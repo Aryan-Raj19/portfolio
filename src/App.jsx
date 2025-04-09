7e66a395e861baf4abc8a937406e3ee7b05ad639
Aryan-Raj19 <aryanraj19715@gmail.com>
2025-04-09 20:11:55 +0530

    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-amber-700 text-4xl text-blue-500 min-h-screen flex items-center justify-center">
+      HII
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        wiggle: "wiggle 0.5s ease-in-out", // Shortened duration for smoother effect
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
